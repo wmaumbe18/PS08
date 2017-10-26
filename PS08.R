cc1f2cbd57a33c5091b036bb786b9c9c2e16f9a6
rudeboybert <albert.ys.kim@gmail.com>
2017-10-26 08:21:50 -0400

    Added PS08.R

---
+++ PS08.R
@@ -0,0 +1,69 @@
+library(tidyverse)
+library(caret)
+
+# Package for easy timing in R
+library(tictoc)
+
+
+
+# Demo of timer function --------------------------------------------------
+# Run the next 5 lines at once
+tic()
+Sys.sleep(3)
+timer_info <- toc()
+runtime <- timer_info$toc - timer_info$tic
+runtime
+
+
+
+# Get data ----------------------------------------------------------------
+# Accelerometer Biometric Competition Kaggle competition data
+# https://www.kaggle.com/c/accelerometer-biometric-competition/data
+train <- read_csv("~/Downloads/train.csv")
+
+# YOOGE!
+dim(train)
+
+
+
+# knn modeling ------------------------------------------------------------
+model_formula <- as.formula(Device ~ X + Y + Z)
+
+# Values to use:
+n_values <- c(10, 20, 30)
+k_values <- c(2, 3, 4)
+
+runtime_dataframe <- expand.grid(n_values, k_values) %>%
+  as_tibble() %>%
+  rename(n=Var1, k=Var2) %>%
+  mutate(runtime = n*k)
+runtime_dataframe
+
+
+
+
+# Time knn here -----------------------------------------------------------
+
+
+
+
+# Plot your results ---------------------------------------------------------
+# Think of creative ways to improve this barebones plot. Note: you don't have to
+# necessarily use geom_point
+runtime_plot <- ggplot(runtime_dataframe, aes(x=n, y=k, col=runtime)) +
+  geom_point()
+
+runtime_plot
+ggsave(filename="firstname_lastname.png", width=16, height = 9)
+
+
+
+
+# Runtime complexity ------------------------------------------------------
+# Can you write out the rough Big-O runtime algorithmic complexity as a function
+# of:
+# -n: number of points in training set
+# -k: number of neighbors to consider
+# -d: number of predictors used? In this case d is fixed at 3
+
+
